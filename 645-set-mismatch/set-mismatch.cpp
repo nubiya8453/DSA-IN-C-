65d292d8c86494a6516644950289c0cb2ce92e04
Nubiya Fathima <nubiyafathima0@gmail.com>
2026-09-13 21:31:28 +0530

    Time: 0 ms (100.00%) | Memory: 26.2 MB (47.47%) - LeetSync

---
+++ 645-set-mismatch/set-mismatch.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>freq(n+1,0);
+        for(int i=0;i<n;i++)
+        {
+            freq[nums[i]]++;
+        }
+        int duplicate,miss;
+        for(int i=1;i<=n;i++)
+        {
+            if( freq[i]==2)
+            duplicate = i;
+            else if( freq[i]==0)
+            miss=i;
+        }
+        //2,3
+      return {duplicate,miss};  
+    }
+};
