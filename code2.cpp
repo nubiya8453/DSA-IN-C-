5cc9cadebe8d0f5204e8a8ec9ea111b920a43f4c
Nubiya Fathima <nubiyafathima0@gmail.com>
2026-09-13 21:29:37 +0530

    Add files via upload

---
+++ code2.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> ans;
+
+        int n = nums.size();
+
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < n; i++) {
+
+            // Skip duplicate values for i
+            if (i > 0 && nums[i - 1] == nums[i])
+                continue;
+
+            int j = i + 1;
+            int k = n - 1;
+
+            while (j < k) {
+
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if (sum < 0) {
+                    j++;
+                }
+                else if (sum > 0) {
+                    k--;
+                }
+                else {
+                    vector<int> temp;
+
+                    temp.push_back(nums[i]);
+                    temp.push_back(nums[j]);
+                    temp.push_back(nums[k]);
+
+                    ans.push_back(temp);
+
+                    // Skip duplicate j values
+                    int left = nums[j];
+                    while (j < k && left == nums[j]) {
+                        j++;
+                    }
+
+                    // Skip duplicate k values
+                    int right = nums[k];
+                    while (j < k && right == nums[k]) {
+                        k--;
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
+};
