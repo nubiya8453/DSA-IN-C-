8306876669264729d75cf8918497ea58fb02aed9
Nubiya Fathima <nubiyafathima0@gmail.com>
2026-09-13 21:38:13 +0530

    Time: 0 ms (100.00%) | Memory: 63 MB (48.90%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int n = height.size();
+        int i=0,j=n-1;
+        int ans=0;
+        
+        while(i<j)
+        {
+            int h = min(height[i],height[j]);
+            int w=j-i;
+           int area= h*w;
+           if(area>ans)  ans=area;
+            
+           if( height[i]<height[j]) i++;
+           else 
+           j--;
+        }
+       return ans; 
+    }
+};
